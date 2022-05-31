418a81a7bfc6527ab6bae300e3e2e0eb2471872f
Jannik <Jannik.Lipke@gmx.de>
2022-05-31 14:55:32 +0200

    Improved Parser

--- simplex-solver/parser.py
+++ simplex-solver/parser.py
@@ -5,7 +5,7 @@ from inspect import _void
 
 benchmark_dir = "./benchmarks"
 
-def getAllFileNames(benchmark_dir) -> list:
+def __getAllFileNames(benchmark_dir) -> list:
     '''
     reads all .txt files in the given directory ./benchmarks and returns a list like this: filename.txt
     '''
@@ -15,7 +15,7 @@ def getAllFileNames(benchmark_dir) -> list:
             files.append(file)
     return files
 
-def getFileContents(benchmark_dir, file_name) -> list:
+def __getFileContents(benchmark_dir, file_name) -> list:
     '''
     parses the file and returns a list of all lines in the file
     '''
@@ -24,8 +24,8 @@ def getFileContents(benchmark_dir, file_name) -> list:
     lines = file.readlines()
     file.close()
     return lines
-
-def parseFileContents(contents : list):
+    
+def __parseFileContents(contents : list):
     '''
     gets the objective function, constraints and if its a minimization or maximization problem.
     '''
@@ -33,11 +33,10 @@ def parseFileContents(contents : list):
     last_row: str = ""
     min_or_max = ""
     for line in contents:
-        print(line)
+        # print(line)
         if line.startswith("//"):
             continue
         if line.startswith(" +") or line.startswith(" -"):
-            print(line)
             row_string.append(line)
         elif line.startswith("min:"):
             min_or_max = "min"
@@ -66,12 +65,23 @@ def parseFileContents(contents : list):
             if(i == len(parts) - 1):
                 rows.append(coefficients)
         
-    print("\n")
-    print(rows)
-    print(min_or_max)
+    # print("\n")
+    # print(rows)
+    # print(min_or_max)
     return rows, min_or_max
 
 
-# filenames = getAllFileNames(benchmark_dir)
-contents = getFileContents(benchmark_dir, "KI_5.txt")
-parseFileContents(contents)
+
+def getParsedBenchmarks() -> list:
+    parsedBenchmarks = []
+    filenames = __getAllFileNames(benchmark_dir)
+    for filename in filenames:
+        print("Parsing file: " + filename)
+        contents = __getFileContents(benchmark_dir, filename)
+        rows, min_or_max = __parseFileContents(contents)
+        print("Ready!\nContents are:")
+        print(rows)
+        print("\n")
+        parsedBenchmarks.append((rows, min_or_max))
+    return parsedBenchmarks
+
